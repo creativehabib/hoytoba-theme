b62e2e2e395442ded2c18fc9a4eaddf700210a3a
Habibur Rahaman <shamolrahaman@gmail.com>
2024-10-11 01:53:46 +0600

    error solve

--- app/recipe/[details]/page.tsx
+++ app/recipe/[details]/page.tsx
@@ -1,17 +1,23 @@
 import RecipeDetailsItem from "@/components/RecipeDetails";
+// Define an interface for the params object
+interface RecipeParams {
+    params: {
+        details: string;
+    };
+}
 
-async function fetchRecipeDetails(currentRecipeId: string) {
+async function fetchRecipeDetails(currentRecipeId: string | undefined) {
     try {
-        const apiResponse = await fetch(`https://dummyjson.com/recipes/${currentRecipeId}`)
+        const apiResponse = await fetch(`https://dummyjson.com/recipes/${currentRecipeId}`);
         return await apiResponse.json();
     } catch (e) {
-        throw new Error(`Failed to fetch recipe details ${e}`);
+        throw new Error(`Failed to fetch recipe details: ${e}`);
     }
 }
 
-export default async function RecipeDetails({params}) {
+export default async function RecipeDetails({params}: RecipeParams) {
     const getRecipeDetails = await fetchRecipeDetails(params?.details);
     return (
-        <RecipeDetailsItem getRecipeDetails={getRecipeDetails}/>
-    )
+        <RecipeDetailsItem getRecipeDetails={getRecipeDetails} />
+    );
 }
